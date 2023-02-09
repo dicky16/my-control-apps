7c06b003be1675a4b4d17a7c7c61e612604b032e
dikky setiwan <setyawandicky88@gmail.com>
2023-02-09 19:23:44 +0700

    add btn

--- app/src/main/java/id/projectin/mycontrol/MainActivity.kt
+++ app/src/main/java/id/projectin/mycontrol/MainActivity.kt
@@ -1,11 +1,27 @@
 package id.projectin.mycontrol
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button:Button
+    private lateinit var tv:TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.btn)
+        tv = findViewById(R.id.textView)
+
+        button.setOnClickListener {
+            tv.text = "Aku Button"
+        }
+
     }
 }
